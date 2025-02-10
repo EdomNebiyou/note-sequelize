f56a90c53c67c03580a7f799709bd5426a79eada
edom nebiyou <edomnebiyou858@gmail.com>
2025-02-11 00:00:44 +0300

    add not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import RootLayout from "./layout/RootLayout"
 import Home from "./pages/Home"
 import NoteDetails from "./pages/NoteDetails"
+import NotFound from "./components/NotFound"
 
 export default function App(){
   const router=createBrowserRouter(
@@ -9,6 +10,7 @@ export default function App(){
       <Route path="/" element={<RootLayout/>}>
         <Route index element={<Home/>}/>
         <Route path="/:id" element={<NoteDetails/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Route>
     )
   )
